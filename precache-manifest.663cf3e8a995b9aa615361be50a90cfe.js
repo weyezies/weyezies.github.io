fbdb88c9df86996270ca977288cf4861aa7c6220
Stage <dev@stageplus.live>
2022-05-21 00:21:36 +0300

    Init

---
+++ precache-manifest.663cf3e8a995b9aa615361be50a90cfe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "0c31cde187a1cb54d1e8",
+    "url": "/static/css/main.f2e75fe0.chunk.css"
+  },
+  {
+    "revision": "0c31cde187a1cb54d1e8",
+    "url": "/static/js/main.0c31cde1.chunk.js"
+  },
+  {
+    "revision": "030cbe5b788297364791",
+    "url": "/static/js/1.030cbe5b.chunk.js"
+  },
+  {
+    "revision": "f0624e90377c6a84b778",
+    "url": "/static/js/2.f0624e90.chunk.js"
+  },
+  {
+    "revision": "4a5c5a0ed68b0dfe6e2f",
+    "url": "/static/js/runtime~main.4a5c5a0e.js"
+  },
+  {
+    "revision": "e63562614cbdc491e4d374f97c513b82",
+    "url": "/static/media/font.e6356261.woff"
+  },
+  {
+    "revision": "0ac390d0ca9a52efda4f8bfa8ea49f2d",
+    "url": "/index.html"
+  }
+];
